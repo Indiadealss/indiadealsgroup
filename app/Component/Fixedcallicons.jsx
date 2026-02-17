7009e457a56b3842a0f0f2142e0cec087f3f4205
Indiadealss <indiadealss1@gmail.com>
2026-02-17 22:42:57 +0530

    add fixed

--- app/Component/Fixedcallicons.jsx
+++ app/Component/Fixedcallicons.jsx
@@ -10,7 +10,10 @@ const Fixedcallicons = () => {
   return (
     <div className='w-15 h-15 rounded-full sticky top-0 bottom-30 right-10 left-full me-3 bg-[#313036] cursor-pointer opacity-80'>
         <div className="w-7 mx-auto pt-5 opacity-85">
-            <FontAwesomeIcon icon={faPhone} className=' text-[#f3e6c6] opacity-85 text-2xl' />
+            <a
+            href="tel:+919818763100"
+            className="bg-[#d2b02d] text-white p-2 rounded"
+          ><FontAwesomeIcon icon={faPhone} className=' text-[#f3e6c6] opacity-85 text-2xl' /></a>
       </div>
     </div>
   )
--- app/Component/Fixedicons.jsx
+++ app/Component/Fixedicons.jsx
@@ -5,10 +5,16 @@ import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
 
 
 const Fixedicons = () => {
+  const message = `Thank you for contacting us.
+  Indiadeals team connect with you as soon as possible`
   return (
     <div className='w-15 h-15 rounded-full sticky top-0 bottom-50 right-10 left-full me-3 bg-green-500 cursor-pointer opacity-80'>
         <div className='w-10 mx-auto pt-2'>
-      <FontAwesomeIcon icon={faWhatsapp}  className='text-white text-4xl text-center' />
+      <a
+  href="https://wa.me/919818763100"
+  target="_blank"
+  rel="noopener noreferrer"
+><FontAwesomeIcon icon={faWhatsapp}  className='text-white text-4xl text-center' /></a>
       </div>
     </div>
   )
