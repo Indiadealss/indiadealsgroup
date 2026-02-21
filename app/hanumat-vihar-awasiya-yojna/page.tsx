956708e51feb131c54ecb059965b35b7da017c31
Indiadealss <indiadealss1@gmail.com>
2026-02-21 11:57:47 +0530

    add color to content

--- app/hanumat-vihar-awasiya-yojna/page.tsx
+++ app/hanumat-vihar-awasiya-yojna/page.tsx
@@ -6,7 +6,7 @@ export const metadata = {
   description: "Hanumat Vihar Awasiya Yojna is a new residential project offers plots and Villas are located at Raal Road, Vrindavan, Mathura. Well connected to Noida, Delhi, Faridabad and Gurgaon.",
 
   alternates: {
-    canonical: "https://indiadealsgroup.com/hanumat-vihar-awasiya-yojna",
+    canonical: "https://www.indiadealsgroup.com/hanumat-vihar-awasiya-yojna",
   },
 
 };
