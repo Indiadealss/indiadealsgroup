698f75f91e7a7166a201198d5e3d36d29b4b46f2
Indiadealss <indiadealss1@gmail.com>
2026-02-21 12:09:25 +0530

    add margin

--- app/Privacy-policy/PrivacypolicyClient.tsx
+++ app/Privacy-policy/PrivacypolicyClient.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image'
 const Privacypolicy = () => {
   return (
     <>
-      <div className='px-5 lg:px-[22%] my-10 '>
+      <div className='px-5 lg:px-[22%] mt-[15vw] md:mt-[10vw] '>
                 <div className='bg-blue-50  py-5 rounded-lg mt-3'>
                     <Image src={privacy} alt='...' className='w-[4%] mx-auto' />
                     <h1 className='text-center mt-3 text-4xl font-bold'><span className='text-center'>Privacy Policy</span></h1>
