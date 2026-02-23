a123d5a15a2b952a82b41ad76d5d4787ad600647
Indiadealss <indiadealss1@gmail.com>
2026-02-23 18:25:20 +0530

    add gtm code .

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ import { Playwrite_NZ } from 'next/font/google';
 import { Dancing_Script } from 'next/font/google'
 import { Cormorant } from 'next/font/google'
 import { Montserrat } from 'next/font/google'
+import Script from "next/script";
 
 const montserrat = Montserrat({
   subsets: ['latin'],
@@ -25,7 +26,7 @@ const playwrite = Playwrite_NZ({
 
 const cormorant = Cormorant({
   subsets: ['latin'],
-  weight: [ '700'],
+  weight: ['700'],
   display: 'swap',
 })
 
@@ -46,12 +47,12 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  icons:{
-    icon:[
-      {url:'/favicon.png', sizes:'32x32', type: 'image/png'},
+  icons: {
+    icon: [
+      { url: '/favicon.png', sizes: '32x32', type: 'image/png' },
     ],
-    shortcut:'/favicon.png',
-    apple:'/favicon.png'
+    shortcut: '/favicon.png',
+    apple: '/favicon.png'
   },
   title: "Top Real Estate Developer in Vrindavan, Mathura, Indiadeals Group",
   description: " Indiadeals Group is a trusted and leading real estate developer launch new residential project Hanumat Vihar Awasiya Yojna offers premium plots, Which is located at Raal Road, Vrindavan, Mathura.",
@@ -69,6 +70,28 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${montserrat.className} antialiased`}>
+
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];
+              w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+              var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+              j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;
+              f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-M5Q5FSL6');
+            `,
+          }}
+        />
+        {/* <!-- Google Tag Manager --> */}
+        {/* <!-- Google Tag Manager (noscript) --> */}
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M5Q5FSL6"
+          height="0" width="0" style={{ display: "none", visibility: "hidden" }}></iframe></noscript>
+        {/* <!-- End Google Tag Manager (noscript) --> */}
         <ClientLayout>{children}</ClientLayout>
       </body>
     </html>
