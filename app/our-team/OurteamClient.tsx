c9e729650fdf695a56eb8e2dd282199b59da45a5
Indiadealss <indiadealss1@gmail.com>
2026-03-07 17:10:44 +0530

    add our team page

--- app/our-team/OurteamClient.tsx
+++ app/our-team/OurteamClient.tsx
@@ -1,6 +1,9 @@
 'use client'
 import Image from 'next/image'
 import teambanner from '@/Images/teamsBanner.jpg';
+import soniMam from '@/Images/Soni-mam-.jpg.jpeg';
+import Deepusir from '@/Images/DeepuSir.jpeg';
+import RichaMam from '@/Images/indiadealsgroupHr.jpeg';
 
 
 const page = () => {
@@ -21,7 +24,60 @@ const page = () => {
             </div>
 
             {/* our team our streanth */}
-            <div className='relative w-full h-[30vw]'>
+            <div className=''>
+                
+
+                <div className=' p-10 bg-opacity-0'>
+                  <div className='mx-auto'>
+                <h1 className='text-center mt-10 font-bold text-xl text-4xl uppercase'>Our Management</h1>
+                <div className='mx-auto w-10 border mt-3'></div>
+                </div>
+                  <div className='shadow-xl md:mx-10 mt-10 py-3    bg-white flex flex-col md:flex-row'>
+                    <div>
+                    <div className='   overflow-hidden ms-4'>
+                      <Image src={soniMam} alt='...'  className='w-80 rounded-t-full'/>
+                    </div>
+                    
+                      <h1 className='text-center uppercase font-black text-xl'>Mrs. Soni Yadav</h1>
+                    </div>
+
+                    <div className='mt-10 ms-10'>
+                      <h1 className='font-bold text-xl'>Director - INDIADEALSGROUP</h1>
+                    </div>
+                    </div>
+
+                    <div className='shadow-xl md:mx-10 mt-10 py-3 my-10    bg-white flex flex-col md:flex-row'>
+                    <div>
+                    <div className='   overflow-hidden ms-4'>
+                      <Image src={Deepusir} alt='...'  className='w-80 rounded-t-full'/>
+                    </div>
+                    
+                      <h1 className='text-center uppercase font-black text-xl'>Mr. Deepu Jaswal</h1>
+                    </div>
+
+                    <div className='mt-10 ms-4'>
+                      <h1 className='font-bold text-xl uppercase'>Managing Director (MD) - INDIADEALSGROUP</h1>
+                    </div>
+                    </div>
+
+
+                    <div className='shadow-xl md:mx-10 mt-10 py-3 my-10    bg-white flex flex-col md:flex-row'>
+                    <div>
+                    <div className='   overflow-hidden ms-4'>
+                      <Image src={RichaMam} alt='...'  className='w-80  rounded-t-full'/>
+                    </div>
+                    
+                      <h1 className='text-center uppercase font-black text-xl'>Mrs. Richa Keshri</h1>
+                    </div>
+
+                    <div className='mt-10 ms-10'>
+                      <h1 className='font-bold text-xl uppercase'>HR Manager - INDIADEALSGROUP</h1>
+                    </div>
+                    </div>
+
+                    
+                </div>
+                
                 
             </div>
     </div>
