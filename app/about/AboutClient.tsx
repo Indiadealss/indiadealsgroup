8abe11e98ab5ce93f5be8198c868cecae209928c
Indiadealss <indiadealss1@gmail.com>
2026-03-11 12:33:54 +0530

    font-popins

--- app/about/AboutClient.tsx
+++ app/about/AboutClient.tsx
@@ -54,7 +54,7 @@ export default function Home() {
       <section className=" px-2 lg:px-20">
       <div className="px-4 lg:px-20">
         <h1><span className="text-[#313036]   text-2xl font-medium']">Since 2017 we have been successful because of these things:</span></h1>
-        <ul className="list-disc space-y-3 text-gray-600 font-[poppins]  ms-10 my-5">
+        <ul className="list-disc space-y-3 text-gray-600   ms-10 my-5">
           <li>We are honest and fair in our business.</li>
           <li>We sell properties that will be worth more money</li>
           <li>We know the market very well</li>
@@ -63,7 +63,7 @@ export default function Home() {
           <li>We are reliable and trustworthy</li>
           </ul>
          <p className="my-10">
-          <span className="py-4 text-gray-600 font-[poppins]">
+          <span className="py-4 text-gray-600 ">
             Our goal is to be a leading real estate company that people trust and like. We want to keep growing and helping people find properties. We will always try to be the best and make our clients happy.
            </span>
         </p>
