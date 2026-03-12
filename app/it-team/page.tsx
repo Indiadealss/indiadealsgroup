5a6575081cdb5d157aed800206f50b6c48c3a955
Indiadealss <indiadealss1@gmail.com>
2026-03-12 18:53:45 +0530

    add all this

--- app/it-team/page.tsx
+++ app/it-team/page.tsx
@@ -1,9 +1,84 @@
 import React from 'react'
+import hcbar from '@/Images/HARI-BHAI.jpg';
+import JwalaPrasadDubey from '@/Images/JwalaPrasadDubey.jpg';
+import Keshav from '@/Images/keshav-bhaiu.jpg'
+import Aanand from '@/Images/Ananad.jpg'
+import Image from 'next/image';
 
 const page = () => {
+    const teamData = [
+        {
+            id: 1,
+            name: "Jwala Prasad Dubey",
+            role: "IT Head",
+            image: JwalaPrasadDubey,
+            linkedin: "#"
+        },
+        {
+            id: 2,
+            name: "H.C. Bairwa",
+            role: "Digital Marketing Manager",
+            image: hcbar,
+            linkedin: "#"
+        },
+        {
+            id: 3,
+            name: "Aanand Singh",
+            role: "Graphics Deginer",
+            image: Aanand,
+            linkedin: "#"
+        },
+        {
+            id: 4,
+            name: "Keshav Yadav",
+            role: "Full Stack Devloper",
+            image: Keshav,
+            linkedin: "#"
+        }
+    ];
   return (
     <div>
+
+
+        <div className='mx-auto'>
+                        <h1 className='text-center mt-10 font-bold text-[#313036] text-xl text-4xl uppercase'>IT Team</h1>
+                        <div className='mx-auto w-10 border mt-3'></div>
+                    </div>
+
+
+      <section className="p-0 m-0 md:py-20 md:px-20">
+                              <div className="grid grid-cols-2 md:grid-cols-4 gap-6 ">
       
+                                  {teamData.map((member) => (
+      
+                                      <div key={member.id} className="bg-white rounded-xl shadow p-4 relative">
+      
+                                          <Image
+                                              src={member.image}
+                                              alt={member.name}
+                                              width={400}
+                                              height={400}
+                                              className="rounded-lg w-full h-[300px] md:h-[320px] object-cover"
+                                          />
+      
+                                          <div className="bg-gray-500/30 backdrop-blur-md shadow rounded-lg px-4 py-3 flex justify-between items-center absolute bottom-6 left-6 right-6">
+      
+                                              <div>
+                                                  <h3 className="font-semibold text-white text-sm">{member.name}</h3>
+                                                  <p className="text-xs text-white">{member.role}</p>
+                                              </div>
+      
+                                              <a href={member.linkedin}>
+                                              </a>
+      
+                                          </div>
+      
+                                      </div>
+      
+                                  ))}
+      
+                              </div>
+                          </section>
     </div>
   )
 }
--- app/our-team/OurteamClient.tsx
+++ app/our-team/OurteamClient.tsx
@@ -2,8 +2,10 @@
 import Image from 'next/image'
 import teambanner from '@/Images/teamsBanner.jpg';
 import soniMam from '@/Images/Soni-mam.webp';
+import saurabh from '@/Images/saurabhYadav.jpg';
 import Deepusir from '@/Images/DeepuSir.webp';
 import RichaMam from '@/Images/richmam.webp';
+
 
 
 const page = () => {
@@ -47,6 +49,8 @@ const page = () => {
             </div>
           </div>
 
+          
+
           <div className='shadow-xl md:mx-10 mt-10 py-3 my-10    bg-white flex flex-col md:flex-row'>
             <div>
               <div className='   overflow-hidden ms-4'>
@@ -66,6 +70,28 @@ const page = () => {
                     Deepu has also completed professional training certifications in real estate, strengthening his expertise in property marketing, legal compliance, and customer relationship management.
                     He is a RERA Registered Real Estate Professional, ensuring transparency, ethical practices, and secure property transactions for clients.
                     With deep market insight, strong leadership, and a vision for growth, Deepu Jaiswal is committed to helping clients find high-value real estate opportunities and secure long-term investment returns.
+                  </span>
+                </p>
+              </div>
+            </div>
+          </div>
+
+
+          <div className='shadow-xl md:mx-10 mt-10 py-3    bg-white flex flex-col md:flex-row'>
+            <div>
+              <div className='   overflow-hidden ms-4'>
+                <Image src={saurabh} alt='...' className='w-80 rounded-t-full' />
+              </div>
+
+              <h1 className='text-center text-[#313036] uppercase font-black text-xl'>Mr. Saurav yadav</h1>
+            </div>
+
+            <div className='mt-10 ms-4'>
+              <h1 className='font-bold text-[#313036] text-xl uppercase'>President - Sales & Marketing</h1><br />
+              <div className='w-[85vw] md:w-[60vw] ms-4 pe-10'>
+                <p className='text-justify text-gray-600'>
+                  <span className='text-justify text-gray-600 leading-6'>
+                    With over 10+ years of dedicated experience in the real estate sector, Saurav  brings a sophisticated, analytical approach to the property market, underpinned by the strategic foundations of an MBA in Marketing from Graphic Era University, Dehradun. Over the past ten years, Saurav has cultivated a reputation for excellence by bridging the gap between complex market dynamics and high-value investment opportunities, ensuring that every client benefits from data-driven insights and meticulous attention to detail. His expertise spans the full spectrum of the industry, specializing in all types of residential and commercial properties—from luxury home acquisitions to high-stakes corporate developments—driven by a commitment to transparency, professional integrity, and long-term asset growth. By leveraging his advanced academic background in marketing and a deep-rooted understanding of regional real estate trends, Saurav provides a seamless, end-to-end consultancy experience that transforms property transactions into strategic legacy-building milestones.
                   </span>
                 </p>
               </div>
