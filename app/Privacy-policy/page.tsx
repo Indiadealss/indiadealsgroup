d15824d4d7f03b3a3a4c90c27669cd36c066fa6d
Indiadealss <indiadealss1@gmail.com>
2026-06-08 20:44:03 +0530

    add

--- app/Privacy-policy/page.tsx
+++ app/Privacy-policy/page.tsx
@@ -3,7 +3,7 @@ import Privacypolicy from "./PrivacypolicyClient";
 
 export const metadata = {
   title: "Privacy-Policy: INDIADEALS GROUP",
-  description: "Learn more about Varnya Group real estate projects.",
+  description: "Learn more about Varnya Group real estate projects. ",
 };
 
 export default function AboutPage() {
